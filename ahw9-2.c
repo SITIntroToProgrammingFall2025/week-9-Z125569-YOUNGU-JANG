2bf84e994d37586e1893656e168f21b0323c0ece
Z125569-YOUNGU-JANG <z125569@shibaura-it.ac.jp>
2025-12-03 16:48:33 +0900

    Update ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a[5];
+	int i, temp;
+	
+	for (i=0; i<5; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	
+	for (i=0; i<5; i++)
+	{
+		if (a[i]>a[i+1])
+		{
+			temp=a[i+1];
+			a[i+1]=a[i];
+			a[i]=temp;
+		}
+	}
+	for (i=0; i<4; i++)
+	{
+		if (a[i]>a[i+1])
+		{
+			temp=a[i+1];
+			a[i+1]=a[i];
+			a[i]=temp;
+		}
+	}for (i=0; i<3; i++)
+	{
+		if (a[i]>a[i+1])
+		{
+			temp=a[i+1];
+			a[i+1]=a[i];
+			a[i]=temp;
+		}
+	}for (i=0; i<2; i++)
+	{
+		if (a[i]>a[i+1])
+		{
+			temp=a[i+1];
+			a[i+1]=a[i];
+			a[i]=temp;
+		}
+	}
+	printf("sorted array:\n");
+
+	for (i=0; i<5; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	return 0;
+}
+
+		
 
