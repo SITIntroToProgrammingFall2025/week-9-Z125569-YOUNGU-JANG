2e6a65f7c21126accd7c0b0d338b7992f260b667
Z125569-YOUNGU-JANG <z125569@shibaura-it.ac.jp>
2025-12-03 16:43:58 +0900

    Update hw9-3.c

--- hw9-3.c
+++ hw9-3.c
@@ -1,1 +1,70 @@
+#include <stdio.h>
+
+int main(void)
+{
+	double a[2][3], b[3][2], c[2][2];
+	int i,j,k;
+	double x,sum=0;
+	
+	for (i=0; i<2; i++)
+	{
+		for (j=0; j<3; j++)
+		{
+			scanf("%lf", &a[i][j]);
+		}
+	}
+	for (i=0; i<3; i++)
+	{
+		for (j=0; j<2; j++)
+		{
+			scanf("%lf", &b[i][j]);
+		}
+	}
+	
+	printf("The first matrix you entered is\n");
+	
+	for (i=0; i<2; i++)
+	{
+		for (j=0; j<3; j++)
+		{
+			printf("%.1f ", a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("The second matrix you entered is\n");
+	
+	for (i=0; i<3; i++)
+	{
+		for (j=0; j<2; j++)
+		{
+			printf("%.1f ", b[i][j]);
+		}
+		printf("\n");
+	}
+	printf("The multiplication product of matrix A and matrix B :\n");
+	
+	for (i=0; i<2; i++)
+	{
+		for (j=0; j<2; j++)
+		{
+			c[i][j]=0.0;
+			for (k=0; k<3; k++)
+			{
+				c[i][j]+=a[i][k]*b[k][j];
+			}
+		}
+	}
+	for (i=0; i<2; i++)
+	{
+		for (j=0; j<2; j++)
+		{
+			printf("%1f ", c[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
 
